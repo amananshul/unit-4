3862e8c636fc578f1989741ddb813bc4111a2ef9
amananshul <80920301+amananshul@users.noreply.github.com>
2022-05-16 23:01:36 +0530

    'ok'

---
+++ lec1 rct201/index.js
@@ -0,0 +1,4 @@
+console.log("Hello Typescript");
+var myname = "Aaman";
+var myAge = 22;
+var isMarried = true;
---
+++ lec1 rct201/index.ts
@@ -0,0 +1,17 @@
+console.log("Hello Typescript");
+const myname:string ="Aaman"
+const myAge:number =22
+const isMarried:boolean =true;
+
+type Human={
+    hasLegal: number;
+    legs:number;
+    veg:boolean;
+}
+type User={
+    userName :"string";
+    age:number;
+    isMarried:boolean;
+    
+    
+}
